141888c4bf12b26127516ac1193ef79959f0a0df
Kaan Berkant Atakişi <159454508+KaanBerkantAtakisi@users.noreply.github.com>
2025-02-20 19:39:29 +0300

    MVC_StokProjesi

---
+++ MvcStok/MvcStok/Controllers/YardimdenemeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcStok.Controllers
+{
+    public class YardimdenemeController : Controller
+    {
+        // GET: Yardimdeneme
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
